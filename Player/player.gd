b62c9903f1df255e2f7a4c3ed102861186ab958e
YeyoGSD <YeyoGSD@proton.me>
2024-07-09 20:48:53 -0700

    Agrega un retorno al salir de la pantalla

--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 signal shot
 
 @onready var shooting_timer:Timer = $ShootingTimer
+@onready var color_rect:ColorRect = $ColorRect #Temp
+@onready var sprite_size:Vector2 = color_rect.get_size() * color_rect.get_scale() #Temp
 
 const ACCELERATION:int = 10
 const MAX_SPEED:int = 300
@@ -12,6 +14,7 @@ var can_shoot:bool = true
 
 func _physics_process(_delta:float) -> void:
 	input_movement()
+	screen_wrap()
 	look_at(get_viewport().get_mouse_position())
 	move_and_slide()
 
@@ -27,6 +30,10 @@ func input_movement() -> void:
 		velocity.limit_length(MAX_SPEED)
 	else:
 		velocity.move_toward(Vector2.ZERO, 3)
+
+func screen_wrap() -> void:
+	global_position.x = wrapf(global_position.x, - sprite_size.x, Global.viewport_size.x + sprite_size.x)
+	global_position.y = wrapf(global_position.y, - sprite_size.y, Global.viewport_size.y + sprite_size.y)
 
 func shoot() -> void:
 	if can_shoot:
