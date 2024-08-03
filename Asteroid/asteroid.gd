05610f3a78ca9d41d09c1e8702069e58d2242d1c
YeyoGSD <YeyoGSD@proton.me>
2024-08-02 22:08:23 -0700

    Cambia el manejo del impulso extra

--- Asteroid/asteroid.gd
+++ Asteroid/asteroid.gd
@@ -8,18 +8,21 @@ enum SIZE {BIG, MEDIUM, SMALL}
 @onready var sprite:Sprite2D = $Sprite
 @onready var collision_shape:CollisionShape2D = $CollisionShape
 
-var velocity:Vector2
-var extra_velocity:Vector2
+const ORIGINAL_SPEED:int = 100
+
+var speed:int
+var direction:Vector2
 var size:SIZE = SIZE.BIG
 
 func _ready() -> void:
-	velocity = Vector2(100, 0).rotated(rotation)
+	speed = ORIGINAL_SPEED
+	direction = Vector2(1, 0).rotated(rotation)
 	if size != SIZE.BIG:
 		set_properties()
 
 func _physics_process(delta:float) -> void:
-	global_position += (velocity + extra_velocity) * delta
-	extra_velocity = extra_velocity.move_toward(Vector2.ZERO, 5)
+	global_position += speed * direction * delta
+	speed = lerp(speed, ORIGINAL_SPEED, 0.1)
 
 func _on_area_entered(_area:Area2D) -> void:
 	destroyed.emit(self)
@@ -29,6 +32,7 @@ func _on_visible_on_screen_notifier_screen_exited() -> void:
 	queue_free()
 
 func set_properties() -> void:
+	speed += 200
 	match size:
 		SIZE.MEDIUM:
 			sprite.scale /= 2
