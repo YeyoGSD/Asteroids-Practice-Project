e27293993d52452a9ea9cb11683a9c07cb1715b5
YeyoGSD <YeyoGSD@proton.me>
2024-08-02 22:45:00 -0700

    Agrega tipado estático

--- PausedMenu/paused_menu.gd
+++ PausedMenu/paused_menu.gd
@@ -1,8 +1,8 @@
 extends Control
 
-func _on_resume_button_pressed():
+func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
 	queue_free()
 
-func _on_exit_button_pressed():
+func _on_exit_button_pressed() -> void:
 	get_tree().quit()
