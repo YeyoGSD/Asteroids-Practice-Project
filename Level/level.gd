61f0942ee036c481a6f59c48658f79a641490ae1
YeyoGSD <YeyoGSD@proton.me>
2024-08-02 22:46:29 -0700

    Agrega partículas al destruir asteroides

--- Level/level.gd
+++ Level/level.gd
@@ -6,6 +6,7 @@ extends Node2D
 
 const BULLET_SCENE:PackedScene = preload("res://Bullet/bullet.tscn")
 const ASTEROID_SCENE:PackedScene = preload("res://Asteroid/asteroid.tscn")
+const EXPLOSION_SCENE:PackedScene = preload("res://ExplosionParticles/explosion.tscn")
 
 func _on_player_shot() -> void:
 	var new_bullet:Bullet = BULLET_SCENE.instantiate()
@@ -23,6 +24,7 @@ func _on_asteroid_spawn_timer_timeout() -> void:
 
 # No creo que sea la mejor manera de hacerlo pero funciona
 func _on_asteroid_destroyed(destroyed_asteroid:Asteroid) -> void:
+	add_explosion(destroyed_asteroid.global_position)
 	if destroyed_asteroid.size == Asteroid.SIZE.SMALL:
 		return
 	
@@ -33,3 +35,9 @@ func _on_asteroid_destroyed(destroyed_asteroid:Asteroid) -> void:
 		new_asteroid.rotate(randf_range(-PI/2, PI/2))
 		new_asteroid.size += destroyed_asteroid.size + 1
 		call_deferred("add_child", new_asteroid)
+
+func add_explosion(spawn_position:Vector2) -> void:
+	var new_explosion:CPUParticles2D = EXPLOSION_SCENE.instantiate()
+	new_explosion.global_position = spawn_position
+	new_explosion.emitting = true
+	add_child(new_explosion)
