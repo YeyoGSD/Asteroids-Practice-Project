facffe9db921918c2d30e9d733024080d0dacc7b
YeyoGSD <YeyoGSD@proton.me>
2024-08-02 22:06:36 -0700

    Pequeña limpieza del código

--- Bullet/bullet.gd
+++ Bullet/bullet.gd
@@ -7,9 +7,14 @@ var direction:Vector2
 var travelled_distance:float = 0
 
 func _physics_process(delta:float) -> void:
-	direction = Vector2.RIGHT.rotated(rotation)
+	move(delta)
+	limit_range(delta)
+
+func move(delta:float) -> void:
+	direction = Vector2.RIGHT.rotated(rotation) # Rotación modificada al instanciarse
 	global_position += direction * SPEED * delta
-	
+
+func limit_range(delta:float) -> void:
 	travelled_distance += SPEED * delta
 	if travelled_distance >= RANGE:
 		queue_free()
--- Level/level.gd
+++ Level/level.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
-@onready var bullet_scene:PackedScene = preload("res://Bullet/bullet.tscn")
-@onready var asteroid_scene:PackedScene = preload("res://Asteroid/asteroid.tscn")
 @onready var player:Player = $Player
 @onready var asteroid_spawn_timer:Timer = $AsteroidSpawnTimer
 @onready var spawn_point:PathFollow2D = $AsteroidSpawnPath/SpawnPoint
 
+const BULLET_SCENE:PackedScene = preload("res://Bullet/bullet.tscn")
+const ASTEROID_SCENE:PackedScene = preload("res://Asteroid/asteroid.tscn")
+
 func _on_player_shot() -> void:
-	var new_bullet:Bullet = bullet_scene.instantiate()
+	var new_bullet:Bullet = BULLET_SCENE.instantiate()
 	new_bullet.global_position = player.global_position
 	new_bullet.global_rotation = player.rotation
 	add_child(new_bullet)
 
 func _on_asteroid_spawn_timer_timeout() -> void:
 	spawn_point.progress_ratio = randf()
-	var new_asteroid:Asteroid = asteroid_scene.instantiate()
+	var new_asteroid:Asteroid = ASTEROID_SCENE.instantiate()
 	new_asteroid.destroyed.connect(_on_asteroid_destroyed)
 	new_asteroid.global_position = spawn_point.global_position
 	new_asteroid.look_at(player.global_position)
@@ -26,10 +27,9 @@ func _on_asteroid_destroyed(destroyed_asteroid:Asteroid) -> void:
 		return
 	
 	for i:int in range(2):
-		var new_asteroid:Asteroid = asteroid_scene.instantiate()
+		var new_asteroid:Asteroid = ASTEROID_SCENE.instantiate()
 		new_asteroid.destroyed.connect(_on_asteroid_destroyed)
 		new_asteroid.global_transform = destroyed_asteroid.global_transform
 		new_asteroid.rotate(randf_range(-PI/2, PI/2))
 		new_asteroid.size += destroyed_asteroid.size + 1
-		new_asteroid.extra_velocity = destroyed_asteroid.velocity.normalized() * 200
 		call_deferred("add_child", new_asteroid)
