eb54c5dbaf33b803644f492d0fc25c67001d1479
YeyoGSD <YeyoGSD@proton.me>
2024-08-08 19:45:38 -0700

    Agrega constante de los colores para explosiones

--- global.gd
+++ global.gd
@@ -2,5 +2,8 @@ extends Node
 
 @onready var viewport_size : Vector2 = get_viewport().get_visible_rect().size
 
+const ASTEROID_EXPLOSION_COLOR:Color = Color(0.60,0.43,0.33)
+const PLAYER_EXPLOSION_COLOR:Color = Color(1,1,1)
+
 var lives:int = 3
 var score:int = 0
